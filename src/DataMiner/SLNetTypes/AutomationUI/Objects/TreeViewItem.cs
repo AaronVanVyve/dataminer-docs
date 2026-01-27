53b582facde09b8bc18965b7a834d4496e43eae7
Aaron Van Vyve <112947232+AaronVanVyve@users.noreply.github.com>
2026-01-27 11:56:18 +0100

    RNs

--- src/DataMiner/SLNetTypes/AutomationUI/Objects/TreeViewItem.cs
+++ src/DataMiner/SLNetTypes/AutomationUI/Objects/TreeViewItem.cs
@@ -47,7 +47,7 @@ namespace Skyline.DataMiner.Net.AutomationUI.Objects
 		/// </summary>
 		/// <value><c>true</c> if the previous collapsed state of the node should be restored; otherwise, <c>false</c>.</value>
 		/// <remarks>
-		/// <note type="note">Available from DataMiner 10.6.3/10.6.0 onwards.</note> <!-- RN 44515 -->
+		/// <note type="note">Available from DataMiner 10.6.3/10.6.0 onwards.</note> <!-- RN 44515 / 44541 -->
 		/// </remarks>
 		public bool UsePreviousCollapsedState { get; set; }
 
